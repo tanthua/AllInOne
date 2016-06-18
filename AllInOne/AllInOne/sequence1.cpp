7d7723adda2687d61f20584c3f64ff83a3a86e7e
thua <tanhua@csus.edu>
2016-06-18 14:27:10 -0700

    update sequence

--- AllInOne/AllInOne/sequence1.cpp
+++ AllInOne/AllInOne/sequence1.cpp
@@ -4,8 +4,15 @@
 
 namespace hua_3
 {
+	//CONSTRUCTOR
+	sequence::sequence() {
+		//empty sequence and no current index
+		current_index = -1;
+		used = 0;
+	}
 	//MODIFICATION MEMBER FUNCTIONS
 	void sequence::start() {
+		//start at zero, if empty, then no current index
 		if (size() > 0) {
 			current_index = 0;
 		}
@@ -15,8 +22,15 @@ namespace hua_3
 	}
 
 	void sequence::advance() {
+		//if there is current index, then move forward
 		if (is_item()) {
-			++current_index;
+			//if current index is the last item, then no more current
+			if (current_index == used) {
+				//last item already.
+            current_index = -1;
+			} else {
+				++current_index;
+			}
 		}
 		else {
 			current_index = -1;
@@ -24,20 +38,42 @@ namespace hua_3
 	}
 
 	void sequence::insert(const value_type& entry) {
+      //can only insert if size < CAP
 		if (size() < CAPACITY) {
-
+			if (is_item()) {
+				//add before current item
+			} else {
+				//no current item, then add at the front		
+            //Shift items 1 index to the right before adding new entry
+            if (size() != 0) {
+               for (size_type i = size() - 1; i >= 0; --i) {
+                  data[i + 1] = data[i];
+               }
+            }
+            data[0] = entry;
+			}
+         ++used;
+		} else {
+         std::cout << "Cannot insert. Running out of CAP\n";
 		}
+
 	}
 
 	void sequence::attach(const value_type& entry) {
-		if (size() < CAPACITY) {
-			if (is_item()) {
-
-			}
-			else {
-
-			}
-		}
+      //can only attach if size < CAP
+      if (size() < CAPACITY) {
+         if (is_item()) {
+            //add before current item
+         }
+         else {
+            //no current item, then add at the end
+            data[used] = entry;
+         }
+      }
+      else {
+         std::cout << "Cannot attach. Running out of CAP\n";
+      }
+      ++used;
 	}
 
 	void sequence::remove_current() {
@@ -59,7 +95,12 @@ namespace hua_3
 			return false;
 		}
 		else {
-			return true;
+         if (current_index >= used) {
+            return false;
+         }
+         else {
+            return true;
+         }
 		}
 	}
 
