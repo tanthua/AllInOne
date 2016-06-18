aebefd17db3d06a4a25bbe84bb720b34a43e4e1f
thua <tanhua@csus.edu>
2016-06-18 11:11:38 -0700

    testing on master computer

--- AllInOne/AllInOne/main.cpp
+++ AllInOne/AllInOne/main.cpp
@@ -7,6 +7,7 @@ int main() {
 	/*cout << "Testing Github Extension" << endl;
 	boost::filesystem::path my_path("C:\\Windows\\System");
 	cout << my_path << endl;*/
+	cout << "Testing on master computer" << endl;
 	cout << "Testing boost and Github extension" << endl;
 	EXIT_SUCCESS;
 	return 0;
