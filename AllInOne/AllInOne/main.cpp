8840a443c1e1e4a17508552d5a1378973bbe39fc
thua <tanhua@csus.edu>
2016-06-20 13:20:44 -0700

    boost is not working well. Back to data structures

--- AllInOne/AllInOne/main.cpp
+++ AllInOne/AllInOne/main.cpp
@@ -1,28 +1,11 @@
+
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
 int main() {
-   char *char_ptr;
-   /*char_ptr = new char[20];
-   for (int i = 0; i < 20; ++i) {
-      char_ptr[i] = 'b';
-   }
-   for (int i = 0; i < 20; ++i) {
-      cout << char_ptr[i] << " ";
-   }*/
-   /*char str[] = "TOUGH TIME DIS YEAR";
-   char_ptr = str;
-   for (int i = 0; i < 10; ++i) {
-      cout << char_ptr[i] << " ";
-   }*/
-
-   //const char str[] = "TOUGH TIME";
-   //char str_1[] = str;
-
-
-   cout << endl;
+   cout << "Boost is not working" << endl;
    EXIT_SUCCESS;
    return 0;
 }
--- AllInOne/AllInOne/poly1.cpp
+++ AllInOne/AllInOne/poly1.cpp
@@ -1,80 +1,80 @@
-#include "poly1.h"
-#include <iostream>
-#include <cstdlib>
-
-using namespace std;
-namespace hua_4
-{
-   //CONSTRUCTORS and DESTRUCTOR
-   //constructor, copy constructor
-   polynomial::polynomial() {
-
-   }
-
-   polynomial::polynomial(const polynomial& source) {
-
-   }
-
-   polynomial::polynomial(double c, unsigned int exponent) {
-
-   }
-
-   //destructor
-   polynomial::~polynomial() {
-
-   }
-
-   //MODIFICATION MEMBER FUNCTIONS
-   void polynomial::add_to_coef(double amount, unsigned int exponent) {
-
-   }
-
-   void polynomial::assign_coef(double coefficient, unsigned int exponent) {
-
-   }
-
-   void polynomial::clear() {
-
-   }
-
-   void polynomial::reserve(size_t number) {
-
-   }
-
-   void polynomial::trim() {
-
-   }
-
-   //MODIFICATION OPERATORS
-   polynomial& polynomial::operator =(const polynomial& source) {
-
-   }
-
- /*  polynomial& polynomial::operator =(double c) {
-
-   }*/
-   polynomial& polynomial::operator +=(const polynomial& p) {
-
-   }
-
-   //polynomial& polynomial::operator +=(double c) {
-
-   //}
-
-   polynomial& polynomial::operator *=(const polynomial& p) {
-
-   }
-
-   polynomial& polynomial::operator *=(double c) {
-
-   }
-
-   // NON-MEMBER INPUT/OUTPUT FUNCTIONS
-   istream& operator >> (istream& in, polynomial& p) {
-
-   }
-
-   ostream& operator << (ostream& out, const polynomial& p) {
-
-   }
-}
+//#include "poly1.h"
+//#include <iostream>
+//#include <cstdlib>
+//
+//using namespace std;
+//namespace hua_4
+//{
+//   //CONSTRUCTORS and DESTRUCTOR
+//   //constructor, copy constructor
+//   polynomial::polynomial() {
+//
+//   }
+//
+//   polynomial::polynomial(const polynomial& source) {
+//
+//   }
+//
+//   polynomial::polynomial(double c, unsigned int exponent) {
+//
+//   }
+//
+//   //destructor
+//   polynomial::~polynomial() {
+//
+//   }
+//
+//   //MODIFICATION MEMBER FUNCTIONS
+//   void polynomial::add_to_coef(double amount, unsigned int exponent) {
+//
+//   }
+//
+//   void polynomial::assign_coef(double coefficient, unsigned int exponent) {
+//
+//   }
+//
+//   void polynomial::clear() {
+//
+//   }
+//
+//   void polynomial::reserve(size_t number) {
+//
+//   }
+//
+//   void polynomial::trim() {
+//
+//   }
+//
+//   //MODIFICATION OPERATORS
+//   polynomial& polynomial::operator =(const polynomial& source) {
+//
+//   }
+//
+// /*  polynomial& polynomial::operator =(double c) {
+//
+//   }*/
+//   polynomial& polynomial::operator +=(const polynomial& p) {
+//
+//   }
+//
+//   //polynomial& polynomial::operator +=(double c) {
+//
+//   //}
+//
+//   polynomial& polynomial::operator *=(const polynomial& p) {
+//
+//   }
+//
+//   polynomial& polynomial::operator *=(double c) {
+//
+//   }
+//
+//   // NON-MEMBER INPUT/OUTPUT FUNCTIONS
+//   istream& operator >> (istream& in, polynomial& p) {
+//
+//   }
+//
+//   ostream& operator << (ostream& out, const polynomial& p) {
+//
+//   }
+//}
