e312a8ece0a87afaf054d3b808bfaacac3cb37fc
thua <tanhua@csus.edu>
2016-06-22 12:09:35 -0700

    started working on polynomial

--- AllInOne/AllInOne/main.cpp
+++ AllInOne/AllInOne/main.cpp
@@ -1,15 +1,10 @@
-#include "mystring.h" 
+#include "poly1.h"
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
 int main() {
-   hua_4::string s1("ABC");
-   hua_4::string s2("def");
-   s1.print_string();
-   hua_4:string ss;
-   ss = s1 + s2;
    EXIT_SUCCESS;
    return 0;
 }
