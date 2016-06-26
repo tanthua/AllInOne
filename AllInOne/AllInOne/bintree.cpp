2d49ad698a43b63d041ba9ea307da6dd637af48f
Tan Hua <tanhua@csus.edu>
2016-06-26 13:56:45 -0700

    working on binary tree node

---
+++ AllInOne/AllInOne/bintree.cpp
@@ -0,0 +1,17 @@
+/*
+bintree template file for bintree header
+3 variables: Item data_field, left and right pointer to the node
+*/
+
+#include "bintree.h"
+#include <cstdlib>
+#include <iostream>
+
+namespace hua_10
+{
+	//NON-MEMBER FUNCTIONS for the binary_tree_node<Item>:
+	template <class Process, BTNode* node_ptr>
+	void inorder(Process f, BTNode* node_ptr) {
+
+	}
+}
