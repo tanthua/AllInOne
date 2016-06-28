72ca2ab1a4cb3d076e1883704b32cdb77edec178
thua <tanhua@csus.edu>
2016-06-28 10:43:08 -0700

    updating binary tree node

--- AllInOne/AllInOne/main.cpp
+++ AllInOne/AllInOne/main.cpp
@@ -10,9 +10,15 @@ int main() {
 
 	binary_tree_node<int>* ptr1;
 	ptr1 = new binary_tree_node<int>(100);
+	binary_tree_node<int>* ptr2;
+	ptr2 = new binary_tree_node<int>(1000);
+	ptr1->set_left(ptr2);
 	cout << "Ptr1: " << ptr1->data() << endl;
+	cout << "Ptr1-left: " << ptr1->left()->data() << endl;
 	binary_tree_node<int>* ptr1_copy = tree_copy(ptr1);
 	cout << "Ptr1_copy: " << ptr1_copy->data() << endl;
+	cout << "Ptr1_copy-left: " << ptr1_copy->left()->data() << endl;
+
 
 	EXIT_SUCCESS;
 	return 0;
