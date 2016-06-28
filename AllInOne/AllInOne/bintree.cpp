88afa4411ec6219bf6d7213a4c8e09930f23cd57
thua <tanhua@csus.edu>
2016-06-28 13:35:18 -0700

    updating tree clear func

--- AllInOne/AllInOne/bintree.cpp
+++ AllInOne/AllInOne/bintree.cpp
@@ -14,6 +14,20 @@ namespace hua_10
 	template class binary_tree_node<int>;
 
 	template <class Item>
+	void tree_clear(binary_tree_node<Item>* root_ptr) {
+		binary_tree_node<Item>* child;
+
+		if (root_ptr != NULL) {
+			child = root_ptr->left();
+			tree_clear(child);
+			child = root_ptr->right();
+			tree_clear(child);
+			delete root_ptr;
+			root_ptr = NULL;
+		}
+	}
+
+	template <class Item>
 	binary_tree_node<Item>* tree_copy(const binary_tree_node<Item>* root_ptr)
 		// Library facilities used: cstdlib
 	{
@@ -21,13 +35,12 @@ namespace hua_10
 		binary_tree_node<Item> *r_ptr;
 
 		if (root_ptr == NULL)
-		return NULL;
+			return NULL;
 		else
 		{
-		l_ptr = tree_copy(root_ptr->left());
-		r_ptr = tree_copy(root_ptr->right());
-		return
-		new binary_tree_node<Item>(root_ptr->data(), l_ptr, r_ptr);
+			l_ptr = tree_copy(root_ptr->left());
+			r_ptr = tree_copy(root_ptr->right());
+			return new binary_tree_node<Item>(root_ptr->data(), l_ptr, r_ptr);
 		}
 		//return new binary_tree_node<Item>(200);
 	}
