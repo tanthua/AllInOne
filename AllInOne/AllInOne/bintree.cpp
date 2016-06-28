850d704b002f7e0cb5c022539804737a38556cd5
thua <tanhua@csus.edu>
2016-06-28 10:10:04 -0700

    fixing template issue

--- AllInOne/AllInOne/bintree.cpp
+++ AllInOne/AllInOne/bintree.cpp
@@ -7,11 +7,29 @@ bintree template file for bintree header
 #include <cstdlib>
 #include <iostream>
 
+
 namespace hua_10
 {
 	//NON-MEMBER FUNCTIONS for the binary_tree_node<Item>:
-	template <class Process, BTNode* node_ptr>
-	void inorder(Process f, BTNode* node_ptr) {
+	template class binary_tree_node<int>;
 
+	template <class Item>
+	binary_tree_node<Item>* tree_copy(const binary_tree_node<Item>* root_ptr)
+		// Library facilities used: cstdlib
+	{
+		binary_tree_node<Item> *l_ptr;
+		binary_tree_node<Item> *r_ptr;
+
+		if (root_ptr == NULL)
+		return NULL;
+		else
+		{
+		l_ptr = tree_copy(root_ptr->left());
+		r_ptr = tree_copy(root_ptr->right());
+		return
+		new binary_tree_node<Item>(root_ptr->data(), l_ptr, r_ptr);
+		}
+		//return new binary_tree_node<Item>(200);
 	}
+
 }
--- AllInOne/AllInOne/main.cpp
+++ AllInOne/AllInOne/main.cpp
@@ -1,21 +1,19 @@
-#include "poly1.h"
-#include "node1.h"
+#include "bintree.h"
+#include "bintree.cpp"
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
-
-void print_node(hua_5::node* ptr) {
-
-	while (ptr != NULL) {
-		cout << ptr->data() << " ";
-		ptr = ptr->link();
-	}
-	cout << endl;
-}
+using namespace hua_10;
 
 int main() {
-	
+
+	binary_tree_node<int>* ptr1;
+	ptr1 = new binary_tree_node<int>(100);
+	cout << "Ptr1: " << ptr1->data() << endl;
+	binary_tree_node<int>* ptr1_copy = tree_copy(ptr1);
+	cout << "Ptr1_copy: " << ptr1_copy->data() << endl;
+
 	EXIT_SUCCESS;
 	return 0;
 }
